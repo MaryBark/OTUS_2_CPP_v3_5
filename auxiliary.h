dbae0598fcbf0eb76192880a1465db08c56c93a4
MaryBark <97518012+MaryBark@users.noreply.github.com>
2023-01-09 18:54:40 +0300

    Update auxiliary.h

--- auxiliary.h
+++ auxiliary.h
@@ -3,14 +3,15 @@
 
 #pragma once
 
+#include <iostream>
 #include <vector>
 #include <functional>
 
-//// Types
+
 
 using func_str = std::function<void(std::string)>;
 
-//// Utils
+
 std::vector<std::string> split(const std::string& str, char d);
 
 void read_lines(std::istream& stream, func_str fn_line_handler);
